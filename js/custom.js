86ee13ca0e0e2ba7a61868d0721c8ec0cd5a4704
PSGO <155865927+PSGO@users.noreply.github.com>
2025-01-31 01:49:27 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,6 @@
  */
 
 // Constants
-
 const corsProxy = 'https://corsproxy.io/?';
 const validMediafireIdentifierDL = /^[a-zA-Z0-9]+$/m;
 const validMediafireShortDL = /^(https?:\/\/)?(www\.)?mediafire\.com\/\?[a-zA-Z0-9]+$/m;
@@ -43,64 +42,37 @@ var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
 var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 // Variables
-
 let validateDelayCheck = null;
 let fromParameters = false;
 
 // Functions
-
-var getQueryStringArray = function() {
-  let assoc=[];
+var getQueryStringArray = function () {
+  let assoc = [];
   let items = window.location.search.substring(1).split('&');
-  for(let j = 0; j < items.length; j++) {
+  for (let j = 0; j < items.length; j++) {
     let a = items[j].split('='); assoc[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
   }
   return assoc;
 };
 
-// normal way to download file
-var downloadFile = function(filePath) {
-  let link=document.createElement('a');
+// Normal way to download file
+var downloadFile = function (filePath) {
+  console.log(`Downloading file from: ${filePath}`);
+  let link = document.createElement('a');
   link.href = filePath;
   link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
+  document.body.appendChild(link);
   link.click();
+  document.body.removeChild(link);
 };
 
-// alternative way when using parameters, to know when the download starts
-var downloadFileStarting = function() {
-  // will try to redirect to previous page or new tab when download starts after a tiny delay
-  setTimeout(function() {
-    // redirect to previous page if it exists
-    if (window.history.length >= 2) window.history.back();
-    else {
-      // redirect to browser specfic newtab
-      if (isSafari) window.location = 'favorites://';
-      else if (isChrome) window.location = 'chrome://newtab';
-      else if (isOpera) window.location = 'opera://newtab';
-      else if (isEdgeChromium) window.location = 'edge://newtab';
-      else if (isEdge || isIE) window.location = 'about:tabs';
-      else if (isFirefox) window.location = 'about:newtab';
-      else window.location = 'about:blank';
-    }
-  }, paramDL_loadDelay);
-};
-var downloadFileBegin = function(filePath) {
-  let iframeDivDL = document.createElement('div');
-  iframeDivDL.style = 'display: none';
-  document.body.appendChild(iframeDivDL);
-
-  let iframeFileDL = '<iframe id="iframeFileDL" src="about:blank" onload="downloadFileStarting()"></iframe>';
-  iframeDivDL.innerHTML = iframeFileDL;
-
-  setTimeout(function() {document.getElementById('iframeFileDL').src = filePath}, paramDL_initialDelay);
-};
-
-var validationChecker = function(url, dlBtn, pInvalid, containedNewUrl, spanMfNewURL) {
+// Validation checker
+var validationChecker = function (url, dlBtn, pInvalid, containedNewUrl, spanMfNewURL) {
   let validatedURL = validMediafireIdentifierDL.test(url) || validMediafireShortDL.test(url) || validMediafireLongDL.test(url);
 
   // Test if the new value is a valid link, to enable the download button
   if (url && validatedURL) {
-    // we have a valid url
+    // We have a valid URL
     if (dlBtn.classList.contains('disable')) dlBtn.classList.remove('disable');
     if (!pInvalid.classList.contains('hide')) pInvalid.classList.add('hide');
     if (containedNewUrl.classList.contains('hide')) containedNewUrl.classList.remove('hide');
@@ -108,7 +80,7 @@ var validationChecker = function(url, dlBtn, pInvalid, containedNewUrl, spanMfNe
 
     return true;
   } else {
-    // need to reset when url isn't valid or no text is entered
+    // Need to reset when URL isn't valid or no text is entered
     if (!dlBtn.classList.contains('disable')) dlBtn.classList.add('disable');
     if (pInvalid.classList.contains('hide')) pInvalid.classList.remove('hide');
     if (!containedNewUrl.classList.contains('hide')) containedNewUrl.classList.add('hide');
@@ -118,9 +90,9 @@ var validationChecker = function(url, dlBtn, pInvalid, containedNewUrl, spanMfNe
   }
 };
 
-// if successful, trys to download file and returns the temporary direct download link as a string
-var attemptDownloadRedirect = async function(url, dlBtn, invalidUrlP, invalidPageP, containerNewUrl, spanMediafireNewUrl) {
-  // in case we are running from the download button
+// Attempt to download file
+var attemptDownloadRedirect = async function (url, dlBtn, invalidUrlP, invalidPageP, containerNewUrl, spanMediafireNewUrl) {
+  // In case we are running from the download button
   if (!url) url = document.getElementById(inputMediafireUrlID).value;
   if (!containerNewUrl) containerNewUrl = document.getElementById(containerNewUrlID);
   if (!spanMediafireNewUrl) spanMediafireNewUrl = document.getElementById(spanMediafireNewUrlID);
@@ -128,55 +100,56 @@ var attemptDownloadRedirect = async function(url, dlBtn, invalidUrlP, invalidPag
   if (!invalidUrlP) invalidUrlP = document.getElementById(pInvalidUrlID);
   if (!invalidPageP) invalidPageP = document.getElementById(pInvalidPageID);
 
-  // reset previous invalid page notice
+  // Reset previous invalid page notice
   if (!invalidPageP.classList.contains('hide')) invalidPageP.classList.add('hide');
 
-  // modify the link to work with proxy
-  url = url.replace('http://', 'https://'); // not required, but makes them secure
-  // if it's just the download identifier, add on mediafire pre-link
+  // Modify the link to work with proxy
+  url = url.replace('http://', 'https://'); // Not required, but makes them secure
+  // If it's just the download identifier, add on Mediafire pre-link
   if (validMediafireIdentifierDL.test(url)) url = 'https://mediafire.com/?' + url;
-  // if the link doesn't have http(s), it needs to be appended
+  // If the link doesn't have http(s), it needs to be appended
   if (!checkHTTP.test(url)) {
     if (url.startsWith('//')) url = 'https:' + url;
     else url = 'https://' + url;
-  };
+  }
 
   console.log(`Checking "${url}" for valid download page...`);
-  // try and get the mediafire page to get actual download link
+  // Try and get the Mediafire page to get actual download link
   try {
-    let mediafirePageResponse = await fetch(corsProxy+encodeURIComponent(url));
-    
-    // make sure the response was ok
+    let mediafirePageResponse = await fetch(corsProxy + encodeURIComponent(url));
+    console.log("Fetch response received:", mediafirePageResponse);
+
+    // Make sure the response was OK
     if (await mediafirePageResponse.ok) {
       let data = await mediafirePageResponse.text();
+      console.log("Page content fetched:", data);
 
-      // if we received a page
+      // If we received a page
       if (data) {
-        // check if download parameter link was instead used on website
+        // Check if download parameter link was instead used on website
         let dlPreUrls = data.match(validMediafirePreDL);
         if (dlPreUrls) {
           let dlPreUrl = dlPreUrls[0];
-          return setTimeout(function() {
+          console.log(`Found pre-download URL: ${dlPreUrl}`);
+          return setTimeout(function () {
             return attemptDownloadRedirect(dlPreUrl, dlBtn, invalidUrlP, invalidPageP, containerNewUrl, spanMediafireNewUrl);
-          }, paramDL_mediafireWebDelay); // delay is required, or else Mediafire's Cloudflare protection will not connect
+          }, paramDL_mediafireWebDelay); // Delay is required, or else Mediafire's Cloudflare protection will not connect
         }
 
-        // we try to find URL by regex matching
+        // We try to find URL by regex matching
         let dlUrls = data.match(validDynamicDL);
         if (dlUrls) {
           let dlUrl = dlUrls[0];
+          console.log(`Found dynamic download URL: ${dlUrl}`);
 
-          console.log(`Downloading from "${dlUrl}"...`);
-          // need to do correct download based on if we came from parameters
-          if (fromParameters) downloadFileBegin(dlUrl);
-          else downloadFile(dlUrl);
-
+          // Trigger download
+          downloadFile(dlUrl);
           return dlUrl;
         }
       }
     }
 
-    // all else should produce an error
+    // All else should produce an error
     console.error(`No valid download button at "${url}".`);
     if (invalidPageP.classList.contains('hide')) invalidPageP.classList.remove('hide');
     if (!containerNewUrl.classList.contains('hide')) containerNewUrl.classList.add('hide');
@@ -185,7 +158,7 @@ var attemptDownloadRedirect = async function(url, dlBtn, invalidUrlP, invalidPag
     return false;
   } catch (err) {
     // There was an error
-    console.warn('Something went wrong.', err);
+    console.error('Something went wrong.', err);
     console.error(`No valid download button at "${url}".`);
     if (invalidPageP.classList.contains('hide')) invalidPageP.classList.remove('hide');
     if (!containerNewUrl.classList.contains('hide')) containerNewUrl.classList.add('hide');
@@ -196,34 +169,32 @@ var attemptDownloadRedirect = async function(url, dlBtn, invalidUrlP, invalidPag
 };
 
 // Wait for page to load
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // Elements
-
   let inputMediafireUrl = document.getElementById(inputMediafireUrlID);
   let containerNewUrl = document.getElementById(containerNewUrlID);
   let spanMediafireNewUrl = document.getElementById(spanMediafireNewUrlID);
   let aMediafireDownloadBtn = document.getElementById(aMediafireDownloadBtnID);
   let pInvalidUrl = document.getElementById(pInvalidUrlID);
   let pInvalidPage = document.getElementById(pInvalidPageID);
-    
-  // Main
 
-  // check URL parameters first
+  // Main
+  // Check URL parameters first
   let paramURL = getQueryStringArray().dl;
   if (paramURL) {
     fromParameters = true;
     inputMediafireUrl.value = paramURL;
     console.log(`Validating "${paramURL}" as valid Mediafire download...`);
-    // run checker once as is necessary
+    // Run checker once as is necessary
     if (validationChecker(paramURL, aMediafireDownloadBtn, pInvalidUrl, containerNewUrl, spanMediafireNewUrl)) {
       attemptDownloadRedirect(paramURL, aMediafireDownloadBtn, pInvalidUrl, pInvalidPage, containerNewUrl, spanMediafireNewUrl);
     }
   }
 
-  // detect any changes to url value
-  inputMediafireUrl.oninput = function() {
-    // needs to be captured before checking since it changes fast
+  // Detect any changes to URL value
+  inputMediafireUrl.oninput = function () {
+    // Needs to be captured before checking since it changes fast
     let currentUrl = inputMediafireUrl.value;
     validationChecker(currentUrl, aMediafireDownloadBtn, pInvalidUrl, containerNewUrl, spanMediafireNewUrl);
-  }
+  };
 });
